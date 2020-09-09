bcde33014470233a4777a24690e21a6d3e250bb3
AmrAhmedA <47694180+AmrAhmedA@users.noreply.github.com>
2020-09-09 22:09:42 +0200

    Updates

---
+++ precache-manifest.83c2890beee540608a947d423fae3f99.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "24e57c8dbe2567492aa9f94bb46996be",
+    "url": "/Gmail-Registration-UI/index.html"
+  },
+  {
+    "revision": "18720ef0482b7ff68959",
+    "url": "/Gmail-Registration-UI/static/css/2.8aa5a7f8.chunk.css"
+  },
+  {
+    "revision": "f1da86f6941bc8f9f5a4",
+    "url": "/Gmail-Registration-UI/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "18720ef0482b7ff68959",
+    "url": "/Gmail-Registration-UI/static/js/2.4483eda7.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/Gmail-Registration-UI/static/js/2.4483eda7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f1da86f6941bc8f9f5a4",
+    "url": "/Gmail-Registration-UI/static/js/main.6cccc9ef.chunk.js"
+  },
+  {
+    "revision": "ded93dcfae325023c0ee",
+    "url": "/Gmail-Registration-UI/static/js/runtime-main.62e158d5.js"
+  },
+  {
+    "revision": "f766a72d334cb6199fff834b4ffb2f08",
+    "url": "/Gmail-Registration-UI/static/media/account.f766a72d.svg"
+  },
+  {
+    "revision": "10cdc2cf98e3a67225ab81530a0aefc5",
+    "url": "/Gmail-Registration-UI/static/media/google_PNG19644.10cdc2cf.png"
+  }
+]);
